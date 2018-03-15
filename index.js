2f48d49b0b153cf5fe2ae791af244ad3aebd1e28
Rauli Laine <rauli.laine@iki.fi>
2018-03-15 23:04:12 +0200

    Implement initial AST transformations

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
+const babel = require('@babel/core');
 const babylon = require('babylon');
 const caporal = require('caporal');
 const fs = require('fs');
+const t = require('@babel/types');
+const traverse = require('@babel/traverse').default;
 
 const resolvedModules = {};
 let entryLevelModule;
 
 const createModule = (path) => {
   const sourceCode = fs.readFileSync(path, 'utf8');
-  const tokens = babylon.parse(sourceCode).tokens;
+  const ast = babylon.parse(sourceCode, { sourceType: 'module' });
   const module = {
+    ast,
     index: Object.keys(resolvedModules).length,
-    path,
-    tokens
+    path
   };
 
   resolvedModules[path] = module;
-  // TODO: Go through imports and resolve them.
+
+  // Go through imports and resolve them.
+  traverse(ast, {
+    enter (path) {
+      if (t.isImportDeclaration(path.node)) {
+        throw new Error('TODO: Convert import statement into require function call');
+      } else if (t.isCallExpression(path.node) && t.isIdentifier(path.node.callee, { name: 'require' })) {
+        let resolvedModule;
+
+        if (path.node.arguments.length !== 1) {
+          throw new Error('TODO: Complain about wrong number of arguments');
+        } else if (!t.isStringLiteral(path.node.arguments[0])) {
+          throw new Error('TODO: Complain about dynamic import');
+        }
+
+        resolvedModule = resolveModule(path.node.arguments[0].value);
+        path.replaceWith(
+          t.callExpression(
+            t.identifier('__minipack_require__'),
+            [t.numericLiteral(resolvedModule.index)]
+          )
+        );
+      } else if (t.isExportAllDeclaration(path.node)) {
+        throw new Error('TODO: Convert export * from "mod"');
+      } else if (t.isExportDefaultDeclaration(path.node)) {
+        throw new Error('TODO: Convert export default foo');
+      } else if (t.isExportNamedDeclaration(path.node)) {
+        throw new Error('TODO: Convert named export');
+      }
+    }
+  });
 
   return module;
 };
